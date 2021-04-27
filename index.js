bb2e632bba38935157871279826f95ecc5dc0053
aungmyatmoe2008 <aungmyatmoe2008@gmail.com>
2021-04-27 16:07:46 +0800

    39_Getting_Post_URLEncoded_values

--- index.js
+++ index.js
@@ -1,46 +1,69 @@
 let http = require('http');
 let url = require('url');
+let qs = require('querystring');
 require('dotenv').config();
+
+let responsor = (consoleResult, req, res, params) => {
+  console.log(consoleResult);
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(params);
+};
 
 let checkURL = {
   GET: {
-    '/': (req, res, params) => {
-      console.log('There is get home method running');
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(
+    '/': (req, res, params) =>
+      responsor(
+        'There is get home method running',
+        req,
+        res,
         `<h1>GET HOME METHOD RUNNING ${
           params.query.name === undefined ? '' : params.query.name
         }</h1>`
-      );
-    },
-    '/about': (req, res, params) => {
-      console.log('There is get about method running');
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(
+      ),
+    '/about': (req, res, params) =>
+      responsor(
+        'There is get about method running',
+        req,
+        res,
         `<h1>GET ABOUT HOME METHOD RUNNING ${
           params.query.name === undefined ? '' : params.query.name
         }</h1>`
-      );
-    },
+      ),
   },
   POST: {
-    '/': (req, res, params) => {
-      console.log('There is post home method running');
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(
+    '/': (req, res, params) =>
+      responsor(
+        'There is post home method running',
+        req,
+        res,
         `<h1>POST HOME METHOD RUNNING ${
           params.query.name === undefined ? '' : params.query.name
         }</h1>`
-      );
-    },
-    '/about': (req, res, params) => {
-      console.log('There is post about method running');
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(
+      ),
+    '/about': (req, res, params) =>
+      responsor(
+        'There is post about method running',
+        req,
+        res,
         `<h1>POST ABOUT METHOD RUNNING ${
           params.query.name === undefined ? '' : params.query.name
         }</h1>`
-      );
+      ),
+    '/api/login': (req, res) => {
+      let body = '';
+      req.on('data', data => {
+        body += data;
+      });
+      req.on('end', () => {
+        let query = qs.parse(body);
+        responsor(
+          `Email is ${query.email} and Password is ${query.password}`,
+          req,
+          res,
+          `<h1>Email is ${query.email} and Password is ${query.password}</h1>`
+        );
+        res.end();
+      });
     },
   },
   NA: (req, res, params) => {
@@ -54,6 +77,7 @@ let startServer = (req, res) => {
   let method = req.method;
   let params = url.parse(req.url, true);
   let resolveRoute = checkURL[method][params.pathname];
+
   if (resolveRoute != null && resolveRoute != undefined) {
     resolveRoute(req, res, params);
   } else {
